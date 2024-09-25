71fa2506c94e226dffede077e210d28b5b489b6c
Pavel <p.suharev@tk-chel.ru>
2024-09-25 15:22:25 +0500

    Add project files.

---
+++ Test/Test/DataTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class DataTest
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<DataTest> Items { get; set; }
+
+        public MainPage()
+        {
+            Items = new List<DataTest>();
+            for (int i = 0; i <= 100; i++)
+            {
+                Items.Add(new DataTest() { Name = "123123123123" });
+            }
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
